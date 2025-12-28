27fb64dceef3ef5aa250cf71b63e421a96dd09ea
Dedust <dedust786@gmail.com>
2025-12-28 18:21:56 +0800

    修改了下枚举类

--- Assets/Scrips/Utilities/Enums.cs
+++ Assets/Scrips/Utilities/Enums.cs
@@ -1,16 +1,33 @@
-public enum NPCState
+public enum NPCState//NPC状态标签
 {
-    Patrol, Chase, Skill
+    Patrol,
+    Chase,
+    Skill
 }
-public enum AttackType
+public enum AttackType//攻击类型标签
 {
-    DownAttack, SwardAttack, CollisionAttack, SwardBash
+    DownAttack, 
+    SwardAttack, 
+    CollisionAttack, 
+    SwardBash
 }
-public enum GroundTypeEnums
+public enum GroundTypeEnums//地面标签
 {
-    grass,dirt,defaultType
+    grass,
+    dirt,
+    defaultType
 }
-public enum CreatureType
+public enum CreatureType//生物标签
 {
-    Player,Monster
+    Player,
+    Monster
+}
+public enum PlayerState {//玩家状态标签
+    Idle,       // 待机
+    Running,    // 移动/奔跑
+    Jumping,    // 跳跃/空中
+    Attacking,  // 普通攻击
+    DownAttack, // 下劈
+    Hurt,       // 受击
+    Dead        // 死亡
 }
